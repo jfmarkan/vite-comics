fc08964e5e667f0feb04bc3cc82d5b302db41086
jfmarkan <jfmarkan@gmail.com>
2023-06-16 17:43:42 +0200

    done!

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -39,7 +39,7 @@ export default {
             color: #FFF;
             padding: .5rem 1rem;
             transform: translate(-50%, -50%);
-            top: 0;
+            top: 400px;
             left: 20%;
         }
     }
