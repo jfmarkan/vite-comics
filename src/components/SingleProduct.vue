1e3bcd3b07367ab5c32a63b20535659ba1425515
jfmarkan <jfmarkan@gmail.com>
2023-06-16 17:17:50 +0200

    Created the Single Product component to create every single object

---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,35 @@
+<template>
+        <article>
+            <img :src="comicElement.thumb" alt="">
+            <p>{{ comicElement.series.toUpperCase() }}</p>
+        </article>
+</template>
+
+<script>
+export default {
+    name: 'SingleProduct',
+    props: {
+        comicElement : Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    article{
+        width: calc((100vw / 9) - 20px);
+        margin-top: 2rem;
+
+        img{
+            width: 100%;
+            height: calc(100vw / 9);
+
+
+            margin-bottom: 1rem;
+        }
+
+        p{
+            color: #FFF;
+            font-size: .9rem;
+        }
+    }
+</style>
